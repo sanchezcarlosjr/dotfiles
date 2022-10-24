ab2d62480f763e26c2babe52f4b73dd46cc7a2b9
sanchezcarlosjr <hello@sanchezcarlosjr.com>
2022-10-23 21:45:27 -0700

    fix: suggestion

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,12 +1,3 @@
-export TERM=xterm
-# ------------------------------------------------------------------------------
-# Powerline theme config
-# ------------------------------------------------------------------------------
-export LANG='en_US.UTF-8'
-export LANGUAGE='en_US:en'
-export LC_ALL='en_US.UTF-8'
-export TERM=xterm-256color
-
 # ------------------------------------------------------------------------------
 # Codely theme config
 # ------------------------------------------------------------------------------
@@ -18,33 +9,41 @@ export CODELY_THEME_PWD_MODE="short" # full, short, home_relative
 # ------------------------------------------------------------------------------
 # Languages
 # ------------------------------------------------------------------------------
+export JAVA_HOME='/Library/Java/JavaVirtualMachines/amazon-corretto-15.jdk/Contents/Home'
+export GEM_HOME="$HOME/.gem"
+export GOPATH="$HOME/.go"
 
 # ------------------------------------------------------------------------------
 # Apps
 # ------------------------------------------------------------------------------
 if [ "$CODELY_THEME_MODE" = "dark" ]; then
-  fzf_colors="pointer:#ebdbb2,bg+:#3c3836,fg:#ebdbb2,fg+:#fbf1c7,hl:#8ec07c,info:#928374,header:#fb4934"
+	fzf_colors="pointer:#ebdbb2,bg+:#3c3836,fg:#ebdbb2,fg+:#fbf1c7,hl:#8ec07c,info:#928374,header:#fb4934"
 else
-  fzf_colors="pointer:#db0f35,bg+:#d6d6d6,fg:#808080,fg+:#363636,hl:#8ec07c,info:#928374,header:#fffee3"
+	fzf_colors="pointer:#db0f35,bg+:#d6d6d6,fg:#808080,fg+:#363636,hl:#8ec07c,info:#928374,header:#fffee3"
 fi
-
-export EDITOR="nvim"
 
 export FZF_DEFAULT_OPTS="--color=$fzf_colors --reverse"
 
 # ------------------------------------------------------------------------------
 # Path - The higher it is, the more priority it has
 # ------------------------------------------------------------------------------
-export path=(
-  "$HOME/bin"
-  "$DOTLY_PATH/bin"
-  "$DOTFILES_PATH/bin"
-  "$HOME/.cargo/bin"
-  "$HOME/lingo19",
-  "/usr/local/bin"
-  "/usr/local/sbin"
-  "/bin"
-  "/usr/bin"
-  "/usr/sbin"
-  "/sbin"
+path+=(
+	"$HOME/bin"
+	"$DOTLY_PATH/bin"
+	"$DOTFILES_PATH/bin"
+	"$JAVA_HOME/bin"
+	"$GEM_HOME/bin"
+	"$GOPATH/bin"
+	"$HOME/.cargo/bin"
+	"/usr/local/opt/ruby/bin"
+	"/usr/local/opt/python/libexec/bin"
+	"/opt/homebrew/bin"
+	"/usr/local/bin"
+	"/usr/local/sbin"
+	"/bin"
+	"/usr/bin"
+	"/usr/sbin"
+	"/sbin"
 )
+
+export path
--- shell/zsh/.zim/init.zsh
+++ shell/zsh/.zim/init.zsh
@@ -1,4 +1,10 @@
 zimfw() { source /home/cest/.dotfiles/shell/zsh/.zim/zimfw.zsh "${@}" }
 zmodule() { source /home/cest/.dotfiles/shell/zsh/.zim/zimfw.zsh "${@}" }
-typeset -g _zim_fpath=()
-
+typeset -g _zim_fpath=(/home/cest/.dotfiles/shell/zsh/.zim/modules/git-info/functions)
+fpath=(${_zim_fpath} ${fpath})
+autoload -Uz -- coalesce git-action git-info
+source /home/cest/.dotfiles/shell/zsh/.zim/modules/environment/init.zsh
+source /home/cest/.dotfiles/shell/zsh/.zim/modules/input/init.zsh
+source /home/cest/.dotfiles/shell/zsh/.zim/modules/completion/init.zsh
+source /home/cest/.dotfiles/shell/zsh/.zim/modules/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /home/cest/.dotfiles/shell/zsh/.zim/modules/zsh-autosuggestions/zsh-autosuggestions.zsh
